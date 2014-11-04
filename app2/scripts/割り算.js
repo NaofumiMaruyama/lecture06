ad2165ecca92b89941df015f3459c58be3c98404
NaofumiMaruyama <maruyamanaofumi@net40-dhcp175.sfc.keio.ac.jp>
2014-11-04 14:39:16 +0900

    lecture06

---
+++ app2/scripts/割り算.js
@@ -0,0 +1,29 @@
+var showAnswer = function(message){
+var output = document.querySelector("#answer");
+output.value = message;
+};
+
+var add = function(supermen){
+var inputA = document.querySelector("#input-a");
+var inputB = document.querySelector("#input-b");
+
+var a = inputA.value;
+var b = inputB.value;
+
+var answer = parseInt(a) / parseInt(b);
+
+console.log("helloWorld: start");
+showAnswer(answer);
+console.log("helloWorld: end");
+};
+
+//　初期設定を行う関数
+var boot = function(supermen){
+console.log("boot: start");
+var button = document.querySelector("button");
+button.addEventListener("click", add);
+console.log("boot: end");
+};
+
+// loadイベント発生時にbootを呼ぶように設定
+window.addEventListener("load", boot);
